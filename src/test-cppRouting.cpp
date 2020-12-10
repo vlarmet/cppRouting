0670b12d0634439f5fcb9b8794435b26d0fb9a1b
Paul Hewson <texhewson@gmail.com>
2020-12-10 15:33:24 +0000

    Remove cout statements
    Remove two cout statements from cpp test file.  This can be built and
    installed locally.
    
    Working on #2

--- src/test-cppRouting.cpp
+++ src/test-cppRouting.cpp
@@ -43,16 +43,14 @@ context("First cppRouting function") {
     Rcpp::NumericVector e = { 0, R_NaReal, R_NaReal, R_NaReal, R_NaReal, R_NaReal, R_NaReal, 6,  0, 10,  4,  5, 0, 17, 11,  0, 15, 16, 0, 2,  4,  6,  0,  1, 0, 9,  3,  5,  7,  0, 0, 15,  9, 11, 13,  6,  0 };
     //Rcpp::NumericVector e = { 0, Rcpp::NA, Rcpp::NA, Rcpp::NA, Rcpp::NA, Rcpp::NA, 6,  0, 10,  4,  5, Rcpp::NA, 17, 11,  0, 15, 16, Rcpp::NA, 2,  4,  6,  0,  1, Rcpp::NA, 9,  3,  5,  7,  0, Rcpp::NA, 15,  9, 11, 13,  6,  0 };
     e.attr("dim") = Rcpp::Dimension(6, 6);
-    Rcpp::NumericMatrix expected = Rcpp::as<Rcpp::NumericMatrix>(e);
+helloI've     Rcpp::NumericMatrix expected = Rcpp::as<Rcpp::NumericMatrix>(e);
     Rcpp::NumericMatrix output( 6, 6 );
     output = InternalDijkstra_mat(gfrom, gto, gw, NbNodes, dep, arr);
     //INFO("NbNodes" << NbNodes << "\n");
-    Rcpp::Rcout << "The value of output : " << output << "\n";
     expect_true(output.nrow() == 6);
     for (int i = 0; i < 6; i++) {
       for (int j = 0; j < 6; j++) {
         expect_true(output(i, j) == expected(i, j));
-        std::cout << expected(i, j) << std::endl;
           }}
   }
 }
